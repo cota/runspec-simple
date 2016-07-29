a9bcad8b86a257f211600cb887397be7b706c677
Emilio G. Cota <cota@braap.org>
2016-07-29 10:26:56 -0400

    grouped_benchmarks: generate dynamically from specint hash
    The static definition does not make sense anymore, since %specint
    is fully populated. Get rid of it.
    
    Signed-off-by: Emilio G. Cota <cota@braap.org>

--- runspec-simple.pl
+++ runspec-simple.pl
@@ -173,11 +173,8 @@ my %specint = (
     },
     );
 
-my %grouped_benchmarks = (
-    'int' => [qw(400.perlbench 401.bzip2 403.gcc 429.mcf 445.gobmk 456.hmmer
-                 458.sjeng 462.libquantum 464.h264ref 471.omnetpp 473.astar
-                 483.xalancbmk)],
-    );
+my %grouped_benchmarks;
+$grouped_benchmarks{int} = [ grep { ! /specrand/ } sort keys %specint ];
 
 my $config = 'x86_64';
 my $help;
